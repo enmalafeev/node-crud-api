5e05a18dccfb555bfccb1ec6ab1e5a04b46d4d86
e.malafeev <e.malafeev@vk.team>
2023-01-03 17:19:20 +0600

    add simple server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log('Hello, world!');
+import http from 'http';
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200);
+  res.end('Hello, World!');
+});
+server.listen(PORT);
