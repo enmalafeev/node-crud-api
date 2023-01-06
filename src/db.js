87a1b6e7e6688bc4dd30715652414634578ff58d
e.malafeev <e.malafeev@vk.team>
2023-01-06 13:19:44 +0600

    update module db

--- src/db.js
+++ src/db.js
@@ -1,3 +1,13 @@
-export const db = {
-  users: []
-};
+export class DataBase {
+  constructor() {
+    this.users = [];
+  }
+
+  get users() {
+    return this.users;
+  }
+
+  set users(user) {
+    this.users.push(user);
+  }
+}
