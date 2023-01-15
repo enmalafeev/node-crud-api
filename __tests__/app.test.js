03b15d324239880b73c17d923ae8bbb763f5753d
e.malafeev <e.malafeev@vk.team>
2023-01-15 15:12:16 +0600

    add test for GET and PUT per id

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -7,6 +7,12 @@ const userData = {
   hobbies: ['skiing']
 };
 
+const updateUser = {
+  username: 'Fred',
+  age: 21,
+  hobbies: []
+};
+
 afterAll((done) => {
   server.close();
   done();
@@ -24,8 +30,33 @@ describe('POST /api/users', () => {
   it('responds with created new user', async () => {
     const response = await request(server).post('/api/users').send(userData);
     expect(response.status).toEqual(201);
+    expect(response.body).toHaveProperty('id');
     expect(response.body.username).toEqual(userData.username);
     expect(response.body.age).toEqual(userData.age);
     expect(response.body.hobbies).toEqual(userData.hobbies);
+  });
+});
+
+describe('GET /api/users/{id}', () => {
+  it('responds with created new user per id', async () => {
+    const response = await request(server).post('/api/users').send(userData);
+    const user = await request(server).get(`/api/users/${response.body.id}`);
+    expect(user.status).toEqual(200);
+    expect(user.body.id).toEqual(response.body.id);
+    expect(user.body.username).toEqual(userData.username);
+    expect(user.body.age).toEqual(userData.age);
+    expect(user.body.hobbies).toEqual(userData.hobbies);
+  });
+});
+
+describe('PUT /api/users/{id}', () => {
+  it('responds with updated user data', async () => {
+    const response = await request(server).post('/api/users').send(userData);
+    const user = await request(server).put(`/api/users/${response.body.id}`).send(updateUser);
+    expect(user.status).toEqual(200);
+    expect(user.body.id).toEqual(response.body.id);
+    expect(user.body.username).toEqual(updateUser.username);
+    expect(user.body.age).toEqual(updateUser.age);
+    expect(user.body.hobbies).toEqual(updateUser.hobbies);
   });
 });
